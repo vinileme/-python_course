b7f530b31ddb83181dc77a657d9b9ab3e6802d90
Vinicius Leme <vinciuslcosta2013@gmail.com>
2024-02-26 16:59:08 -0300

    aula extra e 55 e 56 atualizadas

---
+++ aula_def.py
@@ -0,0 +1,12 @@
+
+
+# def display_message(texto):
+#     print(f'{texto}')
+    
+# display_message('Estou aprendendo sobre funções através do livro de python!')
+
+
+def favorite_book(livro):
+    print(f'Um dos meus livros favoritos é {livro}.')
+    
+favorite_book('Alice no país das maravilhas')
